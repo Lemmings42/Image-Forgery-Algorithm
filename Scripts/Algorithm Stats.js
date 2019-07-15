241e98e3a7d0a27814f9230f4dd3c80dad7fc464
Liam Hillery <ajbubbles2223@gmail.com>
2019-07-15 17:28:29 -0400

    Finished HTML and edited canvas size.

---
+++ Scripts/Algorithm Stats.js
@@ -0,0 +1,7 @@
+let htmlSect = document.createElement('div');
+htmlSect.innerHTML = `\
+<h2 id="topFitness">Best Fitness: 00.00%</h2>\
+<h2 id="avgFitness">Average Fitness: 00.00%</h2>\
+<h2 id="generations">Generations: 0</h2>\
+<h2 id="changes">Changes: 0</h2>\
+`
--- Scripts/Sketch.js
+++ Scripts/Sketch.js
@@ -1,11 +1,44 @@
-function preload(){
+// Built using the p5.js library
+let paused = true;
+let img;
+let pauseButton = document.createElement('button');
+pauseButton.onclick = "pauseToggle();";
+pauseButton.id = "Pause";
+pauseButton.type = "button";
 
+
+
+function preload(){
+  img = loadImage("https://images-na.ssl-images-amazon.com/images/S/influencer-profile-image-prod/logo/thecodingtrain_1549464698899._QL80_._SS205_.png");
 }
 
 function setup(){
-
+  createCanvas(600, 200);
+  background(255, 240, 240);
+  stroke(0);
+  noFill();
+  image(img, 0, 0, 200, 200);
+  image(img, 200, 0, 200, 200);
+  image(img, 400, 0, 200, 200);
+  rect(0, 0, 200, 200);
+  rect(200, 0, 200, 200);
+  rect(400, 0, 200, 200);
+  document.getElementById('body').appendChild(document.createElement("br"));
+  document.getElementById('body').appendChild(pauseButton);
+  document.getElementById('body').appendChild(htmlSect);
 }
 
 function draw(){
-
+  if (!paused){
+    console.log(paused);
+    pauseButton.innerHTML = "Pause";
+    pauseButton.style = "background-color: #f03535";
+  }else{
+    console.log(paused);
+    pauseButton.innerHTML = "Start";
+    pauseButton.style = "background-color: #35f035";
+  }
 }
+pauseButton.addEventListener("mousedown", () => {
+  paused = !paused;
+});
